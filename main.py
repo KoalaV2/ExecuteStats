5a4dd9787120828b4c0ae07b886bff376d0587d7
KoalaV2 <theo.johanson@gmail.com>
2020-12-02 22:09:31 +0100

    made query work

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python3
 
 from influxdb import InfluxDBClient
-hostip = "127.0.0.1"
+hostip = "docker.therepairbear.koala"
 hostport = "8086"
 
 client = InfluxDBClient(host=hostip,port=hostport)
 def createDatabase():
     print("Creating database")
-    client.create_database('database')
+    client.create_database('ExecStats')
 
+#createDatabase()
 client.get_list_database()
-client.switch_database('database')
+client.switch_database('ExecStats')
 print(client.get_list_database())
 json_body = [
     {
         "measurement":"ExcecuteStatus",
         "tags": {
-            "user": "koala",
+            "username": "koala",
             "programName": "hello.py"
             },
         "fields": {
             "exectime": "1m 52s"
-        },
-        "measurement":"ExcecuteStatus",
-        "tags": {
-            "user": "bob",
-            "programName": "program.py"
-        },
-        "fields": {
-            "exectime": "20s"
-            }
-        } 
+        }
+    } 
 ]
 
 print("Writing to DB")
-print(client.write_points(json_body,database='database',protocol=u'json'))
+print(client.write_points(json_body,database='ExecStats',protocol=u'json'))
 print("Querying data from DB")
-print(client.query("SELECT exectime FROM ExcecuteStatus WHERE id='1234'"))
+client.drop_database('database')
+print(client.query("SELECT exectime FROM ExecStats.autogen.ExcecuteStatus WHERE \"username\"='koala' FILL(null)"))
